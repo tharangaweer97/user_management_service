e54bf6ff9a4c6e725bb376ace5b895ce29af8c90
TharangaWeer <tharangaweer321@gmail.com>
2023-12-08 20:22:15 +0530

    java classes_2

--- src/main/java/com/developertracker/usermanagement/controller/UserController.java
+++ src/main/java/com/developertracker/usermanagement/controller/UserController.java
@@ -3,11 +3,12 @@ package com.developertracker.usermanagement.controller;
 import com.developertracker.usermanagement.model.GithubUser;
 import com.developertracker.usermanagement.service.GithubUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-https://api.github.com/repos/slatedocs/slate/contributors
 
 @RestController
 @AllArgsConstructor
@@ -22,7 +23,7 @@ public class UserController {
     }
 
     @GetMapping("/contributors/get-all")
-    public ResponseEntity<List<GitHubContributor>> getAllContributors() {
-        return ResponseEntity.ok(this.gitHubContributorService.getAllContributors());
+    public ResponseEntity<List<GithubUser>> getAllUsers() {
+        return ResponseEntity.ok(this.githubUserService.getAllUsers());
     }
 }
